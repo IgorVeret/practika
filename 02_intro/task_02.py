43e219884d518c3ef212d19418886a969896c644
igor <iveretennikov77@gmail.com>
2022-06-29 19:09:59 +0300

    Задача частично решена. Сделаны арифметические операции и преоразование из алгебраической формы в полярную

--- 02_intro/task_02.py
+++ 02_intro/task_02.py
@@ -13,27 +13,47 @@ class ComplexNumber:
         self.b = b
         self.c = c
         self.d = d
-    def algebra(self):
-        a_first_number = complex(a,b)# алгебраическое первое число
-        a_second_number = complex(c, d)# алгебраическое второе число
+    def complex_number(self):
+        a_first_number = complex(a,b)# первое число
+        a_second_number = complex(c, d)# второе число
         return a_first_number, a_second_number
     def alg_trig(self):
-        t_first_number = complex(math.sqrt(a**2+b**2),math.degrees(math.atan(b/a)))
-        t_second_number= complex(math.sqrt(c**2+d**2),math.degrees(math.atan(d/c)))
-        # print(t_first_number,t_second_number)
+        t_first_number = complex(math.sqrt(a**2+b**2),math.degrees(math.atan(b/a)))# Преобразование в из алгебраического в полярную
+        t_second_number= complex(math.sqrt(c**2+d**2),math.degrees(math.atan(d/c)))# Преобразование в из алгебраического в полярную
         return t_first_number,t_second_number
     def trig_alg(self):
         at_first_number = complex(a*math.cos(b), a*math.sin(b))
         at_second_number= complex(c*math.cos(d), c*math.sin(d))
-        # print(at_first_number, at_second_number)
         return at_first_number, at_second_number
 
 number = ComplexNumber(a,b,c,d)
-alg_1,alg_2= number.algebra()
-z=alg_1.__add__(alg_2)
-print(complex(z))
+alg_1,alg_2= number.complex_number() # Получаем объекты в алгебраической форме
+print('Полученные комплексные числа',alg_1,alg_2)
+add=alg_1.__add__(alg_2)
+sub=alg_1.__sub__(alg_2)
+mul=alg_1.__mul__(alg_2)
+truediv=alg_1.__truediv__(alg_2)
+print('Сложение алгебраических комплексных чисел', add)
+print('Вычитание алгебраических комплексных чисел', sub)
+print('Умножение алгебраических комплексных чисел', mul)
+print('Деление алгебраических комплексных чисел', truediv)
+print()
+print('---------Преобразование в полярную форму------------------------------')
+print()
+alg_1,alg_2= number.alg_trig()
+print('Полученные комплексные числа',alg_1,alg_2)
+add=alg_1.__add__(alg_2)
+sub=alg_1.__sub__(alg_2)
+mul=alg_1.__mul__(alg_2)
+truediv=alg_1.__truediv__(alg_2)
+print('Сложение алгебраических комплексных чисел', add)
+print('Вычитание алгебраических комплексных чисел', sub)
+print('Умножение алгебраических комплексных чисел', mul)
+print('Деление алгебраических комплексных чисел', truediv)
+# print(alg_1,alg_2)
+
 # print(alg_1,alg_2)
 
 # a_first=number.
 
-print(number.trig_alg())
+# print(number.trig_alg())
